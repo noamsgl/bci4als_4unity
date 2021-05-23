c269806a1e8b1612d2f305d78c4be9cd8e0a09e1
Kfir Nissim <kfirr99.gmail.com>
2021-05-23 21:38:15 +0300

    add assets

---
+++ Assets/Scripts/BallScrpt.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScrpt : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    public float throwForce = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        shiftRight(-1);
+        rotateRight(22.5f);
+        throwBall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void throwBall()
+    {
+        m_Rigidbody.AddForce(transform.forward * throwForce, ForceMode.Impulse);
+        foreach (SpriteRenderer guide in GetComponentsInChildren<SpriteRenderer>())
+        {
+            Destroy(guide);
+        }
+    }
+
+    public void shiftRight(float distance = 1)
+    {
+        transform.position += transform.right * distance;
+    }
+    public void rotateRight(float degrees = 45)
+    {
+        transform.Rotate(transform.up, degrees);
+    }
+}
